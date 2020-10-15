d2859e3eb8fadff90b7421ba997fa06fd55ac8fd
thiago-liracst <thiago.lira@ccc.ufcg.edu.br>
2020-10-15 16:50:56 -0300

    Hall Integration

--- frontend/src/pages/Hall/index.js
+++ frontend/src/pages/Hall/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import "./style.css";
 import {
   Table,
@@ -9,25 +9,38 @@ import {
   Button,
 } from "reactstrap";
 
-const Hall = () => {
-  var personalData = JSON.parse(localStorage.getItem("stepone"));
-  var tecnicalData = JSON.parse(localStorage.getItem("steptwo"));
-  var loginData = JSON.parse(localStorage.getItem("stepthree"));
+import api from '../../services/api';
 
-  const originalstringtowpp =
+const Hall = () => {
+  
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    api.get('users').then(response => {
+        setUsers(response.data);
+    })
+}, []);
+
+  function chamaNoZap(numero){
+    const originalstringtowpp =
     "https://wa.me/5583998304290?text=Olá,%20te%20encontrei%20no%20PermutEi%20e%20tenho%20interesse%20pela%20sua%20proposta!";
 
-  const firstarraytostringwpp = originalstringtowpp.split("55");
-  const secondarraytostringwpp = originalstringtowpp.split("?");
+    const firstarraytostringwpp = originalstringtowpp.split("55");
+    const secondarraytostringwpp = originalstringtowpp.split("?");
 
-  const usernumbertonewstringwpp = 83999788428;
+    const usernumbertonewstringwpp = numero;
 
-  const finalarraytotheuserstelephonewpp =
-    firstarraytostringwpp[0] +
-    "55" +
-    usernumbertonewstringwpp +
-    "?" +
-    secondarraytostringwpp[1];
+    const finalarraytotheuserstelephonewpp =
+      firstarraytostringwpp[0] +
+      "55" +
+      usernumbertonewstringwpp +
+      "?" +
+      secondarraytostringwpp[1];
+    
+    console.log(finalarraytotheuserstelephonewpp);
+
+    window.open(finalarraytotheuserstelephonewpp);
+  }
 
   return (
     <Card className="m-3">
@@ -72,249 +85,43 @@ const Hall = () => {
               <th>Nome</th>
               <th>Matrícula</th>
               <th>Batalhão Atual</th>
-              <th>Estado Atual</th>
               <th>Batalhão de Interesse</th>
-              <th>Estado de Interesse</th>
+              <th>Endereco</th>
               <th>Combinar permuta </th>
             </tr>
           </thead>
-          <tbody>
-            <tr>
-              <th scope="row">1</th>
-              <td>DANIEL CASSIANO CHAVES</td>
-              <td>48934-32</td>
-              <td>13º BATALHÃO DA P.M.</td>
-              <td>PARAÍBA</td>
-              <td>14º BATALHÃO DA P.M.</td>
-              <td>PERNAMBUCO</td>
-              <Button
-                style={{
-                  backgroundColor: "yellow",
-                  color: "red",
-                  borderRadius: "18px",
-                  padding: "6px",
-                  textAlign: "center",
-                  boxShadow: "1px 1px 2px #888",
-                }}
-              >
-                <a href={finalarraytotheuserstelephonewpp}>
-                  <i
-                    class="fa fa-whatsapp text-danger"
-                    style={{ fontSize: 24 }}
-                  >
-                    {" "}
-                    PermutEi!
-                  </i>
-                </a>
-              </Button>
-            </tr>
+
+          {users.map((user) => (
+            <tbody>
+              <tr>
+                <th scope="row">{user.id}</th>
+                <td>{user.nome}</td>
+                <td>{user.matricula}</td>
+                <td>{user.batalhao}</td>
+                <td>{user.batalhaointeresse}</td>
+                <td>{user.endereco}</td>
+                <Button
+                  style={{
+                    backgroundColor: "yellow",
+                    color: "red",
+                    borderRadius: "18px",
+                    padding: "6px",
+                    textAlign: "center",
+                    boxShadow: "1px 1px 2px #888",
+                  }}
+                  onClick={() => chamaNoZap(user.numero)}
+                >
+                    <i
+                      class="fa fa-whatsapp text-danger"
+                      style={{ fontSize: 24 }}
+                    >
+                      {" "}
+                      PermutEi!
+                    </i>
+                </Button>
+              </tr>
           </tbody>
-          <tbody>
-            <tr>
-              <th scope="row">2</th>
-              <td>DANIEL CASSIANO CHAVES</td>
-              <td>48934-32</td>
-              <td>13º BATALHÃO DA P.M.</td>
-              <td>PARAÍBA</td>
-              <td>14º BATALHÃO DA P.M.</td>
-              <td>PERNAMBUCO</td>
-              <Button
-                style={{
-                  backgroundColor: "yellow",
-                  color: "red",
-                  borderRadius: "18px",
-                  padding: "6px",
-                  textAlign: "center",
-                  boxShadow: "1px 1px 2px #888",
-                }}
-              >
-                <a href={finalarraytotheuserstelephonewpp}>
-                  <i
-                    class="fa fa-whatsapp text-danger"
-                    style={{ fontSize: 24 }}
-                  >
-                    {" "}
-                    PermutEi!
-                  </i>
-                </a>
-              </Button>
-            </tr>
-          </tbody>
-          <tbody>
-            <tr>
-              <th scope="row">3</th>
-              <td>DANIEL CASSIANO CHAVES</td>
-              <td>48934-32</td>
-              <td>13º BATALHÃO DA P.M.</td>
-              <td>PARAÍBA</td>
-              <td>14º BATALHÃO DA P.M.</td>
-              <td>PERNAMBUCO</td>
-              <link
-                rel="stylesheet"
-                href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css"
-              />
-              <Button
-                style={{
-                  backgroundColor: "yellow",
-                  color: "red",
-                  borderRadius: "18px",
-                  padding: "6px",
-                  textAlign: "center",
-                  boxShadow: "1px 1px 2px #888",
-                }}
-              >
-                <a href={finalarraytotheuserstelephonewpp}>
-                  <i
-                    class="fa fa-whatsapp text-danger"
-                    style={{ fontSize: 24 }}
-                  >
-                    {" "}
-                    PermutEi!
-                  </i>
-                </a>
-              </Button>
-            </tr>
-          </tbody>
-          <tbody>
-            <tr>
-              <th scope="row">4</th>
-              <td>DANIEL CASSIANO CHAVES</td>
-              <td>48934-32</td>
-              <td>13º BATALHÃO DA P.M.</td>
-              <td>PARAÍBA</td>
-              <td>14º BATALHÃO DA P.M.</td>
-              <td>PERNAMBUCO</td>
-              <link
-                rel="stylesheet"
-                href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css"
-              />
-              <Button
-                style={{
-                  backgroundColor: "yellow",
-                  color: "red",
-                  borderRadius: "18px",
-                  padding: "6px",
-                  textAlign: "center",
-                  boxShadow: "1px 1px 2px #888",
-                }}
-              >
-                <a href={finalarraytotheuserstelephonewpp}>
-                  <i
-                    class="fa fa-whatsapp text-danger"
-                    style={{ fontSize: 24 }}
-                  >
-                    {" "}
-                    PermutEi!
-                  </i>
-                </a>
-              </Button>
-            </tr>
-          </tbody>
-          <tbody>
-            <tr>
-              <th scope="row">5</th>
-              <td>DANIEL CASSIANO CHAVES</td>
-              <td>48934-32</td>
-              <td>13º BATALHÃO DA P.M.</td>
-              <td>PARAÍBA</td>
-              <td>14º BATALHÃO DA P.M.</td>
-              <td>PERNAMBUCO</td>
-              <link
-                rel="stylesheet"
-                href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css"
-              />
-              <Button
-                style={{
-                  backgroundColor: "yellow",
-                  color: "red",
-                  borderRadius: "18px",
-                  padding: "6px",
-                  textAlign: "center",
-                  boxShadow: "1px 1px 2px #888",
-                }}
-              >
-                <a href={finalarraytotheuserstelephonewpp}>
-                  <i
-                    class="fa fa-whatsapp text-danger"
-                    style={{ fontSize: 24 }}
-                  >
-                    {" "}
-                    PermutEi!
-                  </i>
-                </a>
-              </Button>
-            </tr>
-          </tbody>
-          <tbody>
-            <tr>
-              <th scope="row">6</th>
-              <td>DANIEL CASSIANO CHAVES</td>
-              <td>48934-32</td>
-              <td>13º BATALHÃO DA P.M.</td>
-              <td>PARAÍBA</td>
-              <td>14º BATALHÃO DA P.M.</td>
-              <td>PERNAMBUCO</td>
-              <link
-                rel="stylesheet"
-                href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css"
-              />
-              <Button
-                style={{
-                  backgroundColor: "yellow",
-                  color: "red",
-                  borderRadius: "18px",
-                  padding: "6px",
-                  textAlign: "center",
-                  boxShadow: "1px 1px 2px #888",
-                }}
-              >
-                <a href={finalarraytotheuserstelephonewpp}>
-                  <i
-                    class="fa fa-whatsapp text-danger"
-                    style={{ fontSize: 24 }}
-                  >
-                    {" "}
-                    PermutEi!
-                  </i>
-                </a>
-              </Button>
-            </tr>
-          </tbody>
-          <tbody>
-            <tr>
-              <th scope="row">7</th>
-              <td>DANIEL CASSIANO CHAVES</td>
-              <td>48934-32</td>
-              <td>13º BATALHÃO DA P.M.</td>
-              <td>PARAÍBA</td>
-              <td>14º BATALHÃO DA P.M.</td>
-              <td>PERNAMBUCO</td>
-              <link
-                rel="stylesheet"
-                href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css"
-              />
-              <Button
-                style={{
-                  backgroundColor: "yellow",
-                  color: "red",
-                  borderRadius: "18px",
-                  padding: "6px",
-                  textAlign: "center",
-                  boxShadow: "1px 1px 2px #888",
-                }}
-              >
-                <a href={finalarraytotheuserstelephonewpp}>
-                  <i
-                    class="fa fa-whatsapp text-danger"
-                    style={{ fontSize: 24 }}
-                  >
-                    {" "}
-                    PermutEi!
-                  </i>
-                </a>
-              </Button>
-            </tr>
-          </tbody>
+          ))}
         </Table>
       </CardBody>
     </Card>
